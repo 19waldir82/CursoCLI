dccf589c8561970ac196204b96d92f042a40003e
Waldir Aparecido da Silva <63414398+19waldir82@users.noreply.github.com>
2025-01-13 18:47:31 -0300

    Add files via upload

--- component1.js
+++ component1.js
@@ -1,21 +1,12 @@
 
 import React from "react"
-import { Text, StyleSheet } from 'react-native'
+import { Text } from 'react-native'
+import estilo from "./estilo"
 
 export default () => {
 
-
-    const styles = StyleSheet.create({
-
-        text: {
-            fontSize: 40,
-            fontWeight: 'bold',
-        }
-    })
-
-
     return(
 
-        <Text style={styles.text}>Aparecido</Text>
+        <Text style={estilo.text}>Aparecido</Text>
     )
 }
