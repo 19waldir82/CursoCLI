5e7b8dcaf780b15fad51b2a4ee9bff0d1227fd74
Waldir Aparecido da Silva <63414398+19waldir82@users.noreply.github.com>
2025-01-13 18:47:50 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,29 +2,29 @@
 import React from "react"
 import { SafeAreaView, Text, StyleSheet } from 'react-native'
 import Component1 from './Components/component1'
+import Comp, { Comp1, Comp2 } from './Components/mult'
+import Estilo from "./Components/estilo"
 
 export default () => {
 
-
-    const styles = StyleSheet.create({
-        view: {
-            flex: 1,
-            backgroundColor: '#eaeaea',
-            justifyContent: 'center',
-            alignItems: 'center'
-        },
-
-        text: {
-            fontSize: 40,
-            fontWeight: 'bold',
-        }
-    })
-
-
     return(
         <SafeAreaView style={styles.view}>
-            <Text style={styles.text}>Waldir</Text>
+            <Text style={Estilo.text}>Waldir</Text>
             <Component1/>
+            <Comp />
+            <Comp1 />
+            <Comp2 />
         </SafeAreaView>
     )
 }
+
+
+const styles = StyleSheet.create({
+
+    view: {
+        flex: 1,
+        backgroundColor: '#eaeaea',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+})
